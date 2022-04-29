31015d29a9839b7bc307f13a4443e557783ee830
Denis Malyshev <malyshev_denis@outlook.com>
2022-04-29 11:33:21 +0400

    commit 1

---
+++ README.md
@@ -0,0 +1,3 @@
+# highlight
+[![Build status](https://ci.appveyor.com/api/projects/status/v6u29wl6b03ro6sd?svg=true)](https://ci.appveyor.com/project/DenGreen/ra-hoc-highlight)
+https://dengreen.github.io/ra_hoc_highlight/
---
+++ src/App.js
@@ -0,0 +1,119 @@
+import React, { useState } from 'react';
+
+function New(props) {
+    return (
+        <div className="wrap-item wrap-item-new">
+            <span className="label">New!</span>
+            {props.children(props.props)}
+        </div>
+    )
+};
+
+function Popular(props) {
+    return (
+        <div className="wrap-item wrap-item-popular">
+            <span className="label">Popular!</span>
+            {props.children(props.props)}
+        </div>
+    )
+};
+
+function WithBlock(Component) {
+    function Wrapper(props) {
+        if(props.views >= 1000) {
+            return(
+                <Popular props={props} children={Component}/>
+            )
+        } else if(props.views <=100) {
+            return(
+                <New props={props} children={Component}/>
+            )
+        }
+        return(
+            <Component {...props}/>
+        )
+    }
+    return Wrapper;
+}
+
+function Article(props) {
+    return (
+        <div className="item item-article">
+            <h3><a href="#">{props.title}</a></h3>
+            <p className="views">Прочтений: {props.views}</p>
+        </div>
+    )
+};
+
+function Video(props) {
+    return (
+        <div className="item item-video">
+            <iframe src={props.url} allow="autoplay; encrypted-media"></iframe>
+            <p className="views">Просмотров: {props.views}</p>
+        </div>
+    )
+};
+
+const WithVideo = WithBlock(Video);
+const WithArticle = WithBlock(Article);
+
+function List(props) {
+    return props.list.map(item => {
+        switch (item.type) {
+            case 'video':
+                return (
+                    <WithVideo {...item} key={item.id}/>
+                );
+
+            case 'article':
+                return (
+                    <WithArticle {...item} key={item.id}/>
+                );
+        }
+    });
+};
+
+export default function App() {
+    const [list, setList] = useState([
+        {
+            id: '1',
+            type: 'video',
+            url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
+            views: 50
+        },
+        {
+            id: '2',
+            type: 'video',
+            url: 'https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0',
+            views: 12
+        },
+        {
+            id: '3',
+            type: 'article',
+            title: 'Невероятные события в неизвестном поселке...',
+            views: 175
+        },
+        {
+            id: '4',
+            type: 'article',
+            title: 'Секретные данные были раскрыты!',
+            views: 1532
+        },
+        {
+            id: '5',
+            type: 'video',
+            url: 'https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0',
+            views: 4253
+        },
+        {
+            id: '6',
+            type: 'article',
+            title: 'Кот Бегемот обладает невероятной...',
+            views: 12,
+        },
+    ]);
+
+    return (
+        <List list={list} />
+    );
+}
